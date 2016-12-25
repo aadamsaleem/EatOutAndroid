e4c4faf6ab04ce1d66d7ff55aba33d1be9c59f74
Aadam Saleem <aadam.saleem@gmail.com>
2016-12-24 19:21:09 -0500

    Update URL

--- app/src/main/java/com/aadamsaleem/eatout/Constants.java
+++ app/src/main/java/com/aadamsaleem/eatout/Constants.java
@@ -7,8 +7,8 @@ package com.aadamsaleem.eatout;
 public class Constants {
 
     //Localhost
-    public static final String BASE_URL = "http://172.16.21.184:8800/eatout/";
-//    public static final String BASE_URL = "http://EatOut-dev.us-west-2.elasticbeanstalk.com/eatout/";
+    public static final String BASE_URL = "http://192.168.0.23:8800/eatout/";
+//public static final String BASE_URL = "http://EatOut-dev.us-west-2.elasticbeanstalk.com/eatout/";
 
     public static final String URL_SIGN_UP = "user/test_login";
 
@@ -30,14 +30,12 @@ public class Constants {
 
     public static final String GET_RESTAURANT_DETAILS = "restaurant/get_restaurant_details";
 
-    public static final String UPDATE_VOTE = "event/update_voteupdate_vote";
+    public static final String UPDATE_VOTE = "event/update_vote";
 
     public static final String URL_GET_ALL_EVENT = "event/getall";
 
     public static final String URL_GET_EVENT_DETAIL = "event/getevent";
 
     public static final String SIGNAL_EVENT_END = "event/end_event";
-
-
 
 }
